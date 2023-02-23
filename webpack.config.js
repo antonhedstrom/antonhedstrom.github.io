fc0d81a2c23312d9df0e5509d9916d5d008e6d10
Anton Hedström <antonhedstrom@gmail.com>
2023-02-23 21:36:28 +0100

    Trying Git custom workflows.

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'docs'), // Github pages only serve from root or docs.
+    path: path.resolve(__dirname, '_build'),
     clean: true,
   },
   devServer: {
